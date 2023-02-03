1469302b771f5790c6ca2b973fd6b3e631f3f1b0
João Pedro <jjoao.monteiro15@gmail.com>
2023-02-02 21:31:16 -0300

    refactor code

--- generics/generics.ts
+++ generics/generics.ts
@@ -1,40 +1,6 @@
 import { Db, MongoClient, Collection } from 'mongodb'
+import { UserRepository } from './repositories/UserRepository'
 
-class UserModel {
-  _id: string
-  name?: string
-}
-
-class MessageModel {
-  userId: string
-  message: string
-}
-
-class BaseRepository<T> {
-  readonly collection: Collection
-  constructor(db: Db, collectionName: string) {
-    this.collection = db.collection(collectionName)
-  }
-
-  async loadById(id: string): Promise<T> {
-    const entity = await this.collection.findOne({_id: id })
-    return entity
-  }
-
-  async loadAll(): Promise<T[]> {
-    const list = await this.collection.find().toArray()
-    return list
-  }
-}
-
-class UserRepository extends BaseRepository<UserModel> {}
-
-class MessageRepository extends BaseRepository<MessageModel> {
-  async getByUser(id: string) {
-    const user = await this.collection.findOne({ _id: id })
-    return user
-  }
-}
 
 class LoginController {
   async login() {
