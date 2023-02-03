1e7179c2c5e9661c1f4d50c6928bff4d79aa1e73
João Pedro <jjoao.monteiro15@gmail.com>
2023-02-02 21:31:06 -0300

    create product repository

---
+++ generics/repositories/ProductRepository.ts
@@ -0,0 +1,4 @@
+import { ProductModel } from "../models/ProductModel";
+import { BaseRepository } from "./base/baseRepository";
+
+export class ProductRepository extends BaseRepository<ProductModel>{}
